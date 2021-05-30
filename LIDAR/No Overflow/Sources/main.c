f9ee16918aeeaf0370fbe81367eae3c150fdc39e
jall3403 <jall3403@uni.sydney.edu.au>
2021-05-31 01:06:09 +1000

    updates

--- LIDAR/No Overflow/Sources/main.c
+++ LIDAR/No Overflow/Sources/main.c
@@ -4,23 +4,19 @@
 
 
 void Init_TC1 (void) {
+  volatile int x;
   // set up the timers for channel 1 to use the interrupt
   TSCR1 = 0x90;             // enables timer requies flag cleared
-  TSCR2 = 0x00;             // prescaler to 1:1
-  TIOS = 0x0;               // enables input capture for TC1
+  TSCR2 = 0x80;             // prescaler to 1:1
+  TIOS =  0x00;               // enables input capture for TC1
   //TCTL4 |= 0x0C;            // sets capture to any edge for PT1
   
-  TIE |= TIE_C1I_MASK;      // enables interrupts for TC1
+  //TIE |= TIE_C1I_MASK;      // enables interrupts for TC1
+  
+  for(x = 0;x<1000;x++) {
+  }
 }
 
-void Init_TC5 (void) {
-  // set up the timers for channel 5 to use the interrupt
-  TSCR1 = 0x80;             // enables timer requies flag cleared
-  TSCR2 = 0x00;             // prescaler to 1:8
-  TIOS |= TIOS_IOS5_MASK;   // enables output compare for TC5
-  TCTL1=0x04;               // sets output to toggle
-  TIE |= TIE_C5I_MASK;      // enables interrupts for TC5
-}
 
 void main(void) {
   /* put your own code here */
@@ -32,19 +28,18 @@ void main(void) {
   DDRH = 0x0;
   
   overflow = 0;
-  TCTL4 = 0x04;
-
-  //TFLG1 = TFLG1_C1F;
+  TFLG1= 0x02;
+  TCTL4 = 0x08;
+                                         
+  TFLG1= 0x02;
   while(TFLG1 != 0x02);
   TFLG2 = 0x80;
-  
-	
-	
   edge1 = TC1;
-  TCTL4 = 0x08;
-
+  
+  TCTL4 = 0x04;
   while(TFLG1 != 0x02);
   edge2 = TC1;
+  
   diff = edge2-edge1;
   //pulse_width = (double)diff/24000000.0; 
   
--- LIDAR/Overflow Interrupt/Sources/main.c
+++ LIDAR/Overflow Interrupt/Sources/main.c
@@ -2,18 +2,18 @@
 #include "derivative.h"      /* derivative-specific definitions */
 
 static unsigned int timer_overflow; //Declare overflow variable
+unsigned int edge1, edge2;
 
 void Init_TC1 (void) {
   // set up the timers for channel 1 to use the interrupt
-volatile int x;
   TSCR1 = 0x90;               // enables timer and fast flag clearing
-  TSCR2 = 0x80;               // prescaler to 1:1, enable overflow interrupt
-  TIOS = 0x00;                // enables input capture for TC1 
-  //TIE |= TIE_C1I_MASK;        // enables interrupts for TC1
+  TIOS = 0x00;                // enables input capture for TC1
+  TSCR2 = 0x00;               // prescaler to 1:1, no overflow interrupt 
+  TIE = 0x02;                 // enables interrupts for TC1
   
-  // Delay to allow prescaler to come into effect
-  for(x = 0;x<1000;x++) {
-  }
+  ICSYS = 0x00;
+  DLYCT = 0x01;
+  ICOVW = 0x02;
 }
 
 interrupt 16 void TOF_ISR(void)
@@ -23,29 +23,60 @@ interrupt 16 void TOF_ISR(void)
     timer_overflow++;
 }
 
+interrupt 9 void PT1_ISR(void)
+{
+  if(TCTL4 == 0x08)
+        {
+          edge1 = TC1;  // read TC1 register for cycle count for when FALLING edge flag was triggered
+          TFLG2 = 0x80; //Clear TOF flag
+          TSCR2 = 0x80; // prescaler to 1:1, enable overflow interrupt
+          TCTL4 = 0x04; //define rising edge trigger
+  
+          while(TFLG1 == 0)
+            {
+            };
+          edge2 = TC1; // read TC1 register for cycle count for when RISING edge flag was triggered
+          PTH = 0x01;  
+        }
+}
+
 void main(void) {
   /* put your own code here */
-  unsigned int edge1, edge2;
   unsigned int diff;
- // double pulse_width;
+  
   Init_TC1();
+  
   timer_overflow = 0; //define overflow starting point
-  
-  DDRH = 0x0;
-  
-  TCTL4 = 0x08; //define falling edge trigger
-  TFLG1=TFLG1 | TFLG1_C1F_MASK; //Clear C1F flag
-  while(TFLG1 != 0x02); //waitng for C1F to trigger interrupt condition flag (falling edge) 
-  TFLG2 = 0x80; //Clear TOF flag
-  edge1 = TC1;  // read TC1 register for cycle count for when FALLING edge flag was triggered
-  
-  TCTL4 = 0x04; //define rising edge trigger
-  //TFLG1=TFLG1 | TFLG1_C1F_MASK;
-  while(TFLG1 != 0x02)  ;//waitng for C1F to trigger interrupt condition flag (rising edge)
-  edge2 = TC1; // read TC1 register for cycle count for when RISING edge flag was triggered
-  diff = edge2-edge1;
+  edge1 = 0;
+  edge2 = 0;
 
   EnableInterrupts;
+  
+  DDRT = 0x00;
+  DDRH = 0x01;
+  
+  TCTL4 = 0x00;
+  PTH = 0x01;   //Pull pin 0 of port H high
+  PTH = 0x00;   //Pull pin 0 of port H low (generate falling edge)
+  TCTL4 = 0x08; //define falling edge trigger
+  //TFLG1 = 0x02;
+  //while(TFLG1 != 0x02); //waitng for C1F to trigger interrupt condition flag (falling 
+  //edge1 = TC1;
+  
+  //DLYCT = 0x00; 
+  //TFLG2 = 0x80; //Clear TOF flag
+  //TSCR2 = 0x80;
+  
+  //edge1 = TC1;  // read TC1 register for cycle count for when FALLING edge flag was triggered
+  //edge1 = TC1H;
+  
+  //TCTL4 = 0x04; //define rising edge trigger
+  //while(TFLG1 != 0x02)  ;//waitng for C1F to trigger interrupt condition flag (rising edge)
+  //edge2 = TC1; // read TC1 register for cycle count for when RISING edge flag was triggered
+  while(edge2 == 0)
+    {
+    };
+  diff = edge2-edge1;
 
   for(;;) {
     _FEED_COP(); /* feeds the dog */
